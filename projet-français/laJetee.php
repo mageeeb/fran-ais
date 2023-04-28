75082a5e619e33c7895ae12d0a076c8cd11c63ba
Magib <112185392+mageeeb@users.noreply.github.com>
2023-04-28 14:36:29 +0200

    sov28_04

---
+++ projet-français/laJetee.php
@@ -0,0 +1,45 @@
+<?php
+require "header.php"
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice Freançais</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1>Exercice Français</h1>
+
+    <section class="page">
+        <h2>Cette Vidéo nous montre :</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/xgnOS77NmcE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <div class="text">
+            <p class="p">Une vieille dame dans sa maison.</p>
+            <p> Le film me plonge dans la peau d’une personne qui voyage dans le temps à partir d’images, pour être à la place des personnages qui se trouvent <br> sur les images et essayer de sentir ce qu’ils ressentent ou de voir ce qu’ils voient. </p>
+           
+            <p> Ces images peuvent être remplies de belles choses, raconter de belles histoires, mais peuvent aussi être tristes et brutales. <br> Certaines images sont si puissantes qu’elles restent avec nous longtemps après les avoir vues.</p>
+           
+        </div>
+    </section>
+    <div class="synt">
+        <h3>Synthèse:</h3>
+        <p>C’est comme si vous aviez vu quelque chose de vraiment spécial et unique. </p>
+    </div>
+
+
+
+
+
+
+
+    <?php include "footer.php" ?>
+</body>
+
+</html>
